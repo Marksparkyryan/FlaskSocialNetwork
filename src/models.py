aba82ce93229075829c4e9ddc24a74d3ef6d3c73
Mark Ryan <markryandeveloper@gmail.com>
2019-08-23 12:16:26 -0400

    fixing socialenv

--- src/models.py
+++ src/models.py
@@ -18,6 +18,15 @@ class User(UserMixin, Model):
         database = DATABASE
         order_by = ("-joined_at",)
 
+    def get_posts(self):
+        return Post.select().where(Post.user == self)
+
+    def get_stream(self):
+        return Post.select().where(
+            (Post.user == self)
+        )
+
+
     @classmethod
     def create_user(cls, username, email, password, admin=False):
         try:
@@ -30,6 +39,18 @@ class User(UserMixin, Model):
         except IntegrityError:
             pass
 
+
+class Post(Model):
+    timestamp = DateTimeField(default=datetime.datetime.now)
+    user = ForeignKeyField(
+       rel_model=User,
+       related_name="posts" 
+    )
+    content = TextField()
+
+    class Meta:
+        database = DATABASE
+        order_by = ("-timestamp",)
 
 def initialize():
     DATABASE.connect()
